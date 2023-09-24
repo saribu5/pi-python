575a58829871f9270237bb9769b1b59a5cb65931
aaron <saribu123456789@gmail.com>
2023-09-24 03:08:33 +0100

    a commitment

--- joystick.py
+++ joystick.py
@@ -3,14 +3,15 @@ import RPi.GPIO as GPIO
 import pigpio
 import time
 
+#quick tutorial time on how to use
+#so plug stuff in and whatever right
+#run command "sudo pigpiod" for the gpio  pins cause you need those
+
+
 servoX = 18
 servoY = 12
 Xduty = 1500
 Yduty = 1500
-
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(servoX, GPIO.OUT)
-#GPIO.setup(servoY, GPIO.OUT)
 
 Xpwm = pigpio.pi()
 Ypwm = pigpio.pi()
@@ -26,7 +27,7 @@ try:
 		print(key)
 		print(val)
 
-		if(key == "x"):
+		if(key == "x" or key == "hat0x"):
 			if(val > 0):
 				Xduty = Xduty + 25
 			elif(val < 0):
@@ -34,12 +35,12 @@ try:
 
 			if(Xduty < 500):
 				Xduty = 500
-			elif(Xduty > 2000):
-				Xduty = 2000
+			elif(Xduty > 2500):
+				Xduty = 2500
 			else:
 				Xpwm.set_servo_pulsewidth(servoX, Xduty)
 
-		elif(key == "y"):
+		elif(key == "y" or key == "hat0y"):
 			if(val > 0):
 				Yduty = Yduty + 25
 			elif(val < 0):
@@ -47,12 +48,16 @@ try:
 
 			if(Yduty < 500):
 				Yduty = 500
-			elif(Yduty > 2000):
-				Yduty = 2000
+			elif(Yduty > 2500):
+				Yduty = 2500
 			else:
 				Ypwm.set_servo_pulsewidth(servoY, Yduty)
+		elif(key == "cross"):
+			Xduty = 1500
+			Yduty = 1500
+			Xpwm.set_servo_pulsewidth(servoX, Xduty)
+			Ypwm.set_servo_pulsewidth(servoY, Yduty)
 
 
 except KeyboardInterrupt:
-		GPIO.cleanup()
-
+	print("interrupt")
